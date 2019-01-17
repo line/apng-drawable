1c247a236b4d0d1458290d128e52890a1690a756
Ralph <git@ralph.ms>
2019-01-17 11:02:14 +0900

    Update to Android Studio 3.3

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -3,7 +3,7 @@ object Versions {
     const val kotlinVersion = "1.3.11"
 
     // Plugin
-    const val androidPluginVersion = "3.3.0-rc03"
+    const val androidPluginVersion = "3.3.0"
     const val ktlintGradleVersion = "6.3.0"
     const val dokkaVersion = "0.9.17"
     const val bintrayGradlePluginVersion = "1.8.4"
