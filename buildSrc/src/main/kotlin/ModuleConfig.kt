d09d74347141711fd55afa443d2d8f0f217c4270
Ralph <hidetsugu.tamaki@linecorp.com>
2021-02-22 14:52:23 +0900

    Prepare for version 1.10.0 release

--- buildSrc/src/main/kotlin/ModuleConfig.kt
+++ buildSrc/src/main/kotlin/ModuleConfig.kt
@@ -2,7 +2,7 @@ object ModuleConfig {
     const val name = "ApngDrawable"
     const val description = "Fast and light weight Animated Portable Network Graphics(APNG) " +
             "image decoder library for Android platform"
-    const val version = "1.10.0-SNAPSHOT"
+    const val version = "1.10.0"
     const val groupId = "com.linecorp"
     const val artifactId = "apng"
     const val siteUrl = "https://github.com/line/apng-drawable"
